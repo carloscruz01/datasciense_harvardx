0656ef0b18210288f3d6405c23332a2934b94a83
Carlos Cruz <110253144+carloscruz01@users.noreply.github.com>
2022-11-09 21:32:11 -0300

    script_bhaskara.R

---
+++ script_bhaskara.r
@@ -0,0 +1,9 @@
+#Criação de objetos
+a <- 2
+b <- 1
+c <- -4
+#Aplicando uma função a estes objetos
+(-b - sqrt(b^2 - 4*a*c)) / (2*a)
+(-b + sqrt(b^2 - 4*a*c)) / (2*a)
+(-b + sqrt(b^2 - 4*a*c)) / (2*a)
+(-b - sqrt(b^2 - 4*a*c)) / (2*a)
