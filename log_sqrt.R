61beb13ed56e3c3836157497d589728713c9497e
Carlos Cruz <110253144+carloscruz01@users.noreply.github.com>
2022-11-09 21:33:14 -0300

    log_sqrt.R

---
+++ log_sqrt.R
@@ -0,0 +1,7 @@
+##ordenando na base 10
+base <- 10
+##Fazendo logaritmo da raiz quadrada de 100 na base 10
+log(sqrt(100), base)
+##Fazendo logaritmo da raiz quadrada de 100 na base 10
+log(sqrt(100), base = 10)
+log(1024, 4)
